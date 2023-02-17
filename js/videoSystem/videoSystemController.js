c3c444be78e895293218aceb7710a999228835f5
Jaime <jaimepovedafernandez@gmail.com>
2023-02-17 01:40:37 +0100

    objetos commit 2

--- js/videoSystem/videoSystemController.js
+++ js/videoSystem/videoSystemController.js
@@ -6,9 +6,15 @@ class VideoSystemController {
     #videoSystemView;
 
     #loadObjects() {
+        let videoSystem = this.#videoSystem;
+
         let category1 = new Category("Acción", "");
         let category2 = new Category("Musical", "");
         let category3 = new Category("Ciencia Ficción", "");
+
+        videoSystem.addCategory(category1);
+        videoSystem.addCategory(category2);
+        videoSystem.addCategory(category3);
 
         let resource1 = new Resource(135, "resource1");
         let resource2 = new Resource(110, "resource2");
@@ -21,9 +27,9 @@ class VideoSystemController {
         let resource9 = new Resource(130, "resource9");
         let resource10 = new Resource(140, "resource10");
         let resource11 = new Resource(145, "resource10");
-        
+
         let resource12 = new Resource(60, "resource6");
-        let resource13= new Resource(65, "resource7");
+        let resource13 = new Resource(65, "resource7");
         let resource14 = new Resource(70, "resource8");
 
         let movie1 = new Movie("Kingsman", "UK", new Date("13/12/2014"), "Una agencia de espías ultra secretos recluta a un joven de la calle en su competitivo programa de entrenamiento, cuando un genio tecnológico amenaza al mundo.", "img/kingsman.jpg", resource1, []);
@@ -38,7 +44,21 @@ class VideoSystemController {
         let movie10 = new Movie("Blade Runner 2049", "EEUU", new Date("06/10/2017"), "Tras la rebelión de los replicantes creados por bioingeniería para ser utilizados como mano de obra esclava y la prohibición a Tyrell Corporation de seguir con su fabricación, el empresario Niander Wallace adquirió lo que quedaba de Tyrell Corp. y creó una nueva línea de replicantes mucho más obedientes. Ahora, en el año 2049, los viejos modelos Nexus 8 que continúan con vida están siendo retirados. Los que les persiguen aún reciben el nombre de Blade Runner.", "img/blade-runner-2049.jpg", resource10, []);
         let movie11 = new Movie("Star Wars: El Despertar de la Fuerza", "EEUU", new Date("18/12/2015"), "Treinta años después de la victoria de la Alianza Rebelde sobre la segunda Estrella de la Muerte, la galaxia tiene que enfrentarse a una nueva amenaza: el malvado Kylo Ren y la Primera Orden. Cuando el desertor Finn llega a un planeta desierto conoce a Rey, cuyo androide contiene un mapa secreto. Juntos, la joven pareja unirá fuerzas con Han Solo para asegurarse de que la resistencia encuentra a Luke Skywalker, el último de los caballeros Jedi.", "img/star-wars-7.jpg", resource11, []);
 
+        videoSystem.addProduction(movie1);
+        videoSystem.addProduction(movie2);
+        videoSystem.addProduction(movie3);
+        videoSystem.addProduction(movie4);
+        videoSystem.addProduction(movie5);
+        videoSystem.addProduction(movie6);
+        videoSystem.addProduction(movie7);
+        videoSystem.addProduction(movie8);
+        videoSystem.addProduction(movie9);
+        videoSystem.addProduction(movie10);
+        videoSystem.addProduction(movie11);
+
         let serie1 = new Serie("Black Mirror", "EEUU", new Date("11/02/2013"), "Black Mirror es una serie de televisión antológica británica de ciencia ficción distópica/costumbrista creada por Charlie Brooker.", "img/black-mirror.jpg", [resource12, resource13, resource14], [], 5);
+
+        videoSystem.addProduction(serie1);
 
         let actor1 = new Person("Taron", "Egerton", "", new Date("10/11/1989"), "img/taron-egerton.jpg");
         let actor2 = new Person("Colin", "Firth", "", new Date("10/09/1960"), "img/colin-firth.jpg");
@@ -55,6 +75,21 @@ class VideoSystemController {
         let actor13 = new Person("Domhall", "Gleeson", "", new Date("12/05/1983"), "img/domhall-gleeson.jpg");
         let actor14 = new Person("Hayley", "Atwell", "", new Date("05/04/1982"), "img/hayley-atwell.jpg");
 
+        videoSystem.addActor(actor1);
+        videoSystem.addActor(actor2);
+        videoSystem.addActor(actor3);
+        videoSystem.addActor(actor4);
+        videoSystem.addActor(actor5);
+        videoSystem.addActor(actor6);
+        videoSystem.addActor(actor7);
+        videoSystem.addActor(actor8);
+        videoSystem.addActor(actor9);
+        videoSystem.addActor(actor10);
+        videoSystem.addActor(actor11);
+        videoSystem.addActor(actor12);
+        videoSystem.addActor(actor13);
+        videoSystem.addActor(actor14);
+
         let director1 = new Person("Matthew", "Vaughn", "", new Date("07/03/1971"), "img/matthew-vaughn.jpg");
         let director2 = new Person("Gavin", "Hood", "", new Date("12/05/1963"), "img/gavin-hood.jpg");
         let director3 = new Person("Sam", "Raimi", "", new Date("23/10/1959"), "img/sam-raimi.jpg");
@@ -67,7 +102,22 @@ class VideoSystemController {
         let director10 = new Person("Owen", "Harris", "", new Date("22/05/1972"), "img/owen-harris.jpg");
         let director11 = new Person("J.J.", "Abrams", "", new Date("27/06/1966"), "img/jj-abrams.jpg");
 
+        videoSystem.addDirector(director1);
+        videoSystem.addDirector(director2);
+        videoSystem.addDirector(director3);
+        videoSystem.addDirector(director4);
+        videoSystem.addDirector(director5);
+        videoSystem.addDirector(director6);
+        videoSystem.addDirector(director7);
+        videoSystem.addDirector(director8);
+        videoSystem.addDirector(director9);
+        videoSystem.addDirector(director10);
+        videoSystem.addDirector(director11);
+
         let user1 = new User("John", "john@gmail.com", "john1234");
+
+        videoSystem.addUser(user1);
+
     }
 
     constructor(model, view) {
